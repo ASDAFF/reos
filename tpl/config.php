2b7dff0db02d55cca5a7e03f143e9ea284ac88be
Webdev <webdev@groupimo.com>
2012-12-12 13:39:05 +0100

    initial commit

---
+++ tpl/config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+//File generated from a txt file at : Wed, 28 Nov 2012 16:11:09 +0100
+
+error_reporting(0);
+
+define("_DB_NAME","reos");
+define("_DB_USER","reos");
+define("_DB_PWD","4dmin*Reos$");
+define("_DB_HOST","91.121.105.18");
+define("_TABLE_PREFIX","tb");
+?>
